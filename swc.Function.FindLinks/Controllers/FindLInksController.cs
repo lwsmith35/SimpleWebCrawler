7840a178afbf2a4b5e4c13a66606fa0d81687f24
Lawrence Smith <lwsmith35@outlook.com>
2020-10-02 10:20:56 -0500

    Drop Orchastration - Decision to embeed in FetchPage.ProcessURL Stub out Async service for FindLInks Function Stub out Async service for StaticContent Function

---
+++ swc.Function.FindLinks/Controllers/FindLInksController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace swc.Function.FindLinks.Controllers
+{
+    [ApiController]
+    [Route("[api/FindLinks")]
+    public class FindLInksController : ControllerBase
+    {
+        private readonly ILogger<FindLInksController> logger;
+
+        public FindLInksController(ILogger<FindLInksController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> ExtractLinksFromPage([FromBody]string PageId)
+        {
+            logger?.LogInformation($"Processing Links for {PageId}");
+            if (false)
+            {
+                return BadRequest("Unable to parse URI");
+            }
+
+            return Accepted();
+            //var pageResult = await pageCollector.SavePageAsync(newPage);
+            // return CreatedAtAction(nameof(GetPageById), new { id = pageResult.Id }, pageResult);
+        }
+    }
+}
---
+++ swc.Function.ProcessStaticContent/Controllers/ProcessStaticContentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace swc.Function.FindLinks.Controllers
+{
+    [ApiController]
+    [Route("[api/ProcessStaticContent")]
+    public class ProcessStaticContentController : ControllerBase
+    {
+        private readonly ILogger<ProcessStaticContentController> logger;
+
+        public ProcessStaticContentController(ILogger<ProcessStaticContentController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> ProcessStaticContent([FromBody]string PageId)
+        {
+            logger?.LogInformation($"Processing Static Content for {PageId}");
+            if (false)
+            {
+                return BadRequest("Unable to parse URI");
+            }
+
+            return Accepted();
+            //var pageResult = await pageCollector.SavePageAsync(newPage);
+            // return CreatedAtAction(nameof(GetPageById), new { id = pageResult.Id }, pageResult);
+        }
+    }
+}
